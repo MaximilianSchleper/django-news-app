8ee2fbe9bbf10bebe2bf70ce5ebe6f0c847e8922
max schleper <mx.schleper@gmail.com>
2019-04-12 11:59:52 +0200

    new article tests

--- articles/tests.py
+++ articles/tests.py
@@ -2,21 +2,20 @@ from django.test import TestCase
 from django.urls import reverse
 
 # Create your tests here.
-class ArcticlePageTests(TestCase):
+class ArcticlePageTestsLoggedOut(TestCase):
 
-  def test_articles_page_status_code_when_logged_out(self):
+  def test_articles_page_status_code(self):
     response = self.client.get('/articles')
     self.assertEqual(response.status_code, 301)
 
   def test_view_url_by_name(self):
     response = self.client.get(reverse('article_list'))
-    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.status_code, 302)
 
-  def test_view_uses_correct_template(self):
-    response = self.client.get(reverse('article_list'))
-    self.assertEqual(response.status_code, 200)
-    self.assertTemplateUsed(response, 'article_list.html')
-  
+  def test_article_creation_page_status_code(self):
+    response = self.client.get(reverse('article_new'))
+    self.assertEqual(response.status_code, 302)
+
 # class ArticleCRUDTests(TestCase):
   # todo: test article CRUD
 
