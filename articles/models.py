6d9410b9566a69cd9bc48d9014c2318b03b084a4
max schleper <mx.schleper@gmail.com>
2019-04-13 20:19:38 +0200

    comments

--- articles/models.py
+++ articles/models.py
@@ -8,10 +8,7 @@ class Article(models.Model):
   title = models.CharField(max_length=255)
   body = models.TextField()
   date = models.DateTimeField(auto_now_add=True)
-  author = models.ForeignKey(
-    get_user_model(),
-    on_delete=models.CASCADE,
-  )
+  author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
   def __str__(self):
     return self.title
@@ -19,3 +16,17 @@ class Article(models.Model):
   def get_absolute_url(self):
       return reverse("article_detail", args=[str(self.id)])
   
+class Comment(models.Model):
+  article = models.ForeignKey(
+    Article,
+    on_delete=models.CASCADE,
+    related_name='comments'
+  )
+  comment = models.CharField(max_length=240)
+  author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.comment
+
+  def get_absolute_url(self):
+      return reverse("article_list")
